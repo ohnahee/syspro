dd20e3c701dd89a4b86c11ad4653e3e86fbf6689
hee <ohnahee0824@gmail.com>
2024-11-20 11:39:30 +0000

    chap9 prob6

---
+++ chap9/prob6/pgrp1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    int pid, gid;
+
+    printf("PARENT: PID = %d GID = %d\n", getpid(), getpgrp());
+    pid = fork();
+    if (pid == 0) {
+        printf("CHILD: PID = %d GID = %d\n", getpid(), getpgrp());
+    }
+
+    return 0;
+}
+
